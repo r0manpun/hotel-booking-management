c61e9f5b1dbc7a61164d66f14ee298f474cd53cc
r0manpun <punroman112@gmail.com>
2025-05-17 07:46:31 +0545

    feat(styling): revamp Cabin Ui with styled-components

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,15 +8,36 @@ import CabinEditForm from './CabinEditForm';
 
 import { formatCurrency } from '../../utils/helper';
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.6rem 2.4rem;
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-slate-100);
+const Img = styled.img`
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3/2;
+  object-fit: cover;
+  object-position: center;
+
+  &:hover {
+    transform: scale(1.5) translateX(-7px);
   }
+`;
+
+const Cabin = styled.div`
+  font-family: 'sono';
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-slate-600);
+`;
+
+const Price = styled.div`
+  font-family: 'sono';
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+const Discount = styled.div`
+  font-family: 'sono';
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: var(--color-green-700);
 `;
 
 function CabinRow({ cabin }) {
@@ -31,14 +52,18 @@ function CabinRow({ cabin }) {
 
   return (
     <Table.Row>
-      <img
+      <Img
         src={image ? image : null}
         alt={name}
       />
-      <div>{name}</div>
-      <div>Fits up to {maxCapacity}</div>
-      <div>{formatCurrency(regularPrice)}</div>
-      {discount ? <div>{formatCurrency(discount)}</div> : <span>&mdash;</span>}
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      {discount ? (
+        <Discount>{formatCurrency(discount)}</Discount>
+      ) : (
+        <span>&mdash;</span>
+      )}
 
       <Modal>
         <Menus.Menu>
