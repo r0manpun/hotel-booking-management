1ec24a72de151e6934f17ba0b787e7f742d9fa23
r0manpun <punroman112@gmail.com>
2025-05-24 18:16:35 +0545

    feat(api): add getBooking api

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -10,9 +10,24 @@ export async function getBookings() {
 
   if (error) {
     console.error(error.message);
-    throw new Error(error.message);
+    throw new Error('Failed to fetch bookings');
   }
   console.log(data);
+
+  return data;
+}
+
+export async function getBooking(id) {
+  const { data, error } = await supabase
+    .from('bookings')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    console.error(error.message);
+    throw new Error('Failed to fetch booking');
+  }
 
   return data;
 }
