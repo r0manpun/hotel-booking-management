1535ea4cba1e25bbae33d3804ff39b63421b950e
r0manpun <punroman112@gmail.com>
2025-06-08 07:37:21 +0545

    feat: install recharts for Data visualization

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
 import { getStaysAfterDate } from '../../services/apiBookings';
+import { subDays } from 'date-fns';
 
 export function useRecentStays() {
   const [searchParams] = useSearchParams();
@@ -10,8 +11,9 @@ export function useRecentStays() {
     : Number(searchParams.get('last'));
 
   const queryDate = subDays(new Date(), numDays).toISOString();
+
   const { isLoading, data: stays } = useQuery({
-    queryFn: getStaysAfterDate(),
+    queryFn: () => getStaysAfterDate(queryDate),
     queryKey: ['stays', `last-${numDays}`],
   });
 
